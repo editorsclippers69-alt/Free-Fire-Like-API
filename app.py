248024982eaf5ad7c9f9acdf39193943d1feebf4
editorsclippers69-alt <editorsclippers69@thethinkschool.com>
2026-04-09 13:28:27 +0530

    app.py

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ def make_request(encrypt, server_name, token):
             'Expect': "100-continue",
             'X-Unity-Version': "2018.4.11f1",
             'X-GA': "v1 1",
-            'ReleaseVersion': "OB52"
+            'ReleaseVersion': "OB53"
         }
         response = requests.post(url, data=edata, headers=headers, verify=False)
         hex_data = response.content.hex()
